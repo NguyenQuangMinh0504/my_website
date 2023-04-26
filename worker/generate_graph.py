1c3de533da70febd42940adee89e08760313f405
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-04-26 08:37:09 +0700

    Update

--- worker/generate_graph.py
+++ worker/generate_graph.py
@@ -4,15 +4,16 @@ start_date = datetime(year=2023, month=4, day=10)
 end_date = datetime.now()
 date_list = []
 running_list = [2, 3.02, 4.02, 3.55, 3.03, 4.76, 3.02, 2.77, 2.67,
-                3.17, 3.45, 3.42, 4.54, 5.3, ]
-while start_date <= end_date:
-    date_list.append(start_date)
+                3.17, 3.45, 3.42, 4.54, 5.3, 2.86, 3.68]
+while start_date.day < end_date.day:
+    # date_list.append(start_date.strftime("%a %d-%m"))
+    date_list.append(start_date.strftime("%d"))
     start_date += timedelta(days=1)
 plt.plot(date_list, running_list)
 plt.savefig("../running/static/running/images/distance.png")
 plt.clf()
 running_time_list = [30, 30, 30, 30, 30, 45, 30, 30,
-                     30, 30, 30, 30, 45, 45, ]
+                     30, 30, 30, 30, 45, 45, 30, 30]
 pace_list = []
 for i in range(len(running_list)):
     pace_list.append(running_time_list[i] / running_list[i])
