a6aab2ffadbc4a69340baf5660d39fd628a2915b
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-04-26 09:44:28 +0700

    something

--- running/views.py
+++ running/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .data import running_list
 
 # Create your views here.
 
--- worker/generate_graph.py
+++ worker/generate_graph.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+
+from running.data import running_list, running_time_list
+
 start_date = datetime(year=2023, month=4, day=10)
 end_date = datetime.now()
 date_list = []
-running_list = [2, 3.02, 4.02, 3.55, 3.03, 4.76, 3.02, 2.77, 2.67,
-                3.17, 3.45, 3.42, 4.54, 5.3, 2.86, 3.68]
 while start_date.day < end_date.day:
     # date_list.append(start_date.strftime("%a %d-%m"))
     date_list.append(start_date.strftime("%d"))
@@ -12,8 +13,6 @@ while start_date.day < end_date.day:
 plt.plot(date_list, running_list)
 plt.savefig("../running/static/running/images/distance.png")
 plt.clf()
-running_time_list = [30, 30, 30, 30, 30, 45, 30, 30,
-                     30, 30, 30, 30, 45, 45, 30, 30]
 pace_list = []
 for i in range(len(running_list)):
     pace_list.append(running_time_list[i] / running_list[i])
