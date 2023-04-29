e1444db2be62c815d3976659a2440fd58d911ede
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-04-29 10:38:16 +0700

    .

--- running/generate_graph.py
+++ running/generate_graph.py
@@ -6,7 +6,7 @@ from data import running_list, running_time_list
 start_date = datetime(year=2023, month=4, day=10)
 end_date = datetime.now()
 date_list = []
-while start_date.day < end_date.day:
+while start_date.day <= end_date.day:
     # date_list.append(start_date.strftime("%a %d-%m"))
     date_list.append(start_date.strftime("%d"))
     start_date += timedelta(days=1)
