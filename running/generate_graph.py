2d1f16dcfeeec35d867cb7ab5f65670a862f9b66
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-04-29 21:15:28 +0700

    .

--- running/generate_graph.py
+++ running/generate_graph.py
@@ -1,14 +1,22 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-
-from data import running_list, running_time_list
+from redis import Redis
+import json
+redis_client = Redis(host="127.0.0.1", port=6379, db=0)
 
 start_date = datetime(year=2023, month=4, day=10)
 end_date = datetime.now()
 date_list = []
+running_list = []
+running_time_list = []
+
 while start_date.day <= end_date.day:
-    # date_list.append(start_date.strftime("%a %d-%m"))
     date_list.append(start_date.strftime("%d"))
+    running_data = json.loads(
+        redis_client.get(start_date.strftime("%x")).decode("utf-8")
+        )
+    running_list.append(running_data["distance"])
+    running_time_list.append(running_data["time"])
     start_date += timedelta(days=1)
 plt.plot(date_list, running_list)
 plt.savefig("../running/static/running/images/distance.png")
