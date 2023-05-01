a4df2dcc683289d7ba1d9881e8b65987e129f079
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-01 15:32:07 +0700

    Update views.py

--- running/views.py
+++ running/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .data import running_list
+from redis import Redis
+redis_client = Redis(host="127.0.0.1", db=0, port=6379)
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "running/index.html",
-                  {"consecutive_day": len(running_list)})
+                  {"consecutive_day": len(redis_client.keys())})
