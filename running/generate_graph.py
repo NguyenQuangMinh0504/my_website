76d275ca886537a098a86e23b5588676cf2a1a7a
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-16 16:21:25 +0700

    .

--- running/generate_graph.py
+++ running/generate_graph.py
@@ -6,6 +6,7 @@ from settings import PROJECT_PATH
 
 
 def generate_graph():
+    
     start_date = datetime(year=2023, month=4, day=10)
     end_date = datetime.now()
     date_list = []
@@ -18,8 +19,9 @@ def generate_graph():
             running_data = json.loads(running_data.decode("utf-8"))
             running_list.append(running_data["distance"])
             running_time_list.append(running_data["time"])
-            date_list.append(start_date.strftime("%d"))
+            date_list.append(start_date)
         start_date += timedelta(days=1)
+
     plt.plot(date_list, running_list)
     plt.savefig(f"{PROJECT_PATH}/running/static/running/images/distance.png")
     plt.clf()
