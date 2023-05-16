0cef4064f17f1cb1ced38059c2c298b2d06e9e33
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-17 00:28:40 +0700

    .

--- running/generate_graph.py
+++ running/generate_graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from datetime import datetime, timedelta
 import json
 from db import running_table
@@ -19,19 +20,24 @@ def generate_graph():
             running_data = json.loads(running_data.decode("utf-8"))
             running_list.append(running_data["distance"])
             running_time_list.append(running_data["time"])
-            date_list.append(start_date)
+            date_list.append(start_date.strftime("%x"))
         start_date += timedelta(days=1)
-
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
     plt.plot(date_list, running_list)
     plt.savefig(f"{PROJECT_PATH}/running/static/running/images/distance.png")
     plt.clf()
     pace_list = []
     for i in range(len(running_list)):
         pace_list.append(running_time_list[i] / running_list[i])
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
     plt.plot(date_list, pace_list)
     plt.axhline(y=8, color="red", linestyle="dashed")
     plt.savefig(f"{PROJECT_PATH}/running/static/running/images/pace.png")
     plt.clf()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
     plt.bar(date_list, running_time_list)
     plt.savefig(f"{PROJECT_PATH}/running/static/running/images/duration.png")
 
