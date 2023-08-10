890a07f3047072a552d704e47efd9b58360cd6f9
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-08-10 15:48:09 +0700

    Del: Xoá database

--- db.py
+++ db.py
@@ -2,12 +2,4 @@ from redis import Redis
 from mysql import connector
 cnx = connector.connect(user="root", password="qmqmqm8c3", database="my_data")
 cursor = cnx.cursor()
-
-
 running_table = Redis(host="127.0.0.1", port=6379, db=0)
-
-
-user_table = [
-    {"username": "nguyenquangminh", "password": "123", "cookie": "cookie1"},
-    {"username": "nguyenminh2", "password": "1234", "cookie": "cookie2"},
-]
