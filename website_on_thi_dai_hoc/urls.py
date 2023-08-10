0bb1c2f634fbf80f732c1f3b14fe3017c1214f7c
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-08-10 15:47:44 +0700

    Del: Xoá các API đăng nhập, đăng ký, đăng xuất, statistic.

--- website_on_thi_dai_hoc/urls.py
+++ website_on_thi_dai_hoc/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import (register, login, logout,
-                    blog, blog_detail, about_me, homepage, useful_link)
+from .views import (blog, blog_detail, about_me, homepage, useful_link)
 
 urlpatterns = [
     path(route="", view=homepage, name="homepage"),
-    path(route="login/", view=login, name="login"),
-    path(route="register/", view=register, name="register"),
-    path(route="logout/", view=logout, name="logout"),
     path('admin/', admin.site.urls),
     path("running/", include("running.urls")),
     path(route="blog/", view=blog, name="blog"),
