debf8728138555fb6934c4528bbc7a4a6467dbb4
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-08-11 10:36:34 +0700

    Del: Bỏ db redis không dùng.

--- db.py
+++ db.py
@@ -1,5 +1,3 @@
-from redis import Redis
 from mysql import connector
 cnx = connector.connect(user="root", password="qmqmqm8c3", database="my_data")
 cursor = cnx.cursor()
-running_table = Redis(host="127.0.0.1", port=6379, db=0)
