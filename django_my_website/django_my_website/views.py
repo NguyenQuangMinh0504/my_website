7220b1cb375a7cb15875086a7726078c2f3a8660
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-12-04 10:48:00 +0700

    Update views.py

--- django_my_website/django_my_website/views.py
+++ django_my_website/django_my_website/views.py
@@ -182,7 +182,7 @@ def test_view(request: HttpRequest):
 @csrf_exempt
 def github_view(request: HttpRequest):
     data = request.POST
-    print(data["payload"]["commits"])
+    print(data["payload"][0]["commits"][0])
     if "django_my_website/requirements.txt" in data["payload"]["commits"][0]["modified"]:
         send_telegram_notification(message="It worked!!!")
     return HttpResponse(content="Hello")
