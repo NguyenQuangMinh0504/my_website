ff74f0054f8ba4a7b6151330aca2c036dedd0029
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-01-24 13:15:11 +0700

    Update settings.py

--- django_my_website/django_my_website/settings.py
+++ django_my_website/django_my_website/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = 'https://saugau.com/static/'
+# STATIC_URL = 'https://saugau.com/static/'
+STATIC_URL = 'https://saugau.edge.vccloud.vn/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
