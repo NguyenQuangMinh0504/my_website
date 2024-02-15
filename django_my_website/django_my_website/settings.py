ef2670b64bd407299f667421702ef3b64e7c0db4
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-15 10:47:16 +0700

    .

--- django_my_website/django_my_website/settings.py
+++ django_my_website/django_my_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5=z-r%09b%dpyg^jn)pbmcfr1gg@i8&xyr-ioe9d2sxci+^au'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["saugau.com", "localhost"]
 
