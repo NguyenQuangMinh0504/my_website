48ebd61c2bf8220ecc539bf97551cad842ee83bd
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-15 14:09:28 +0700

    .

--- django_my_website/blog/models.py
+++ django_my_website/blog/models.py
@@ -15,3 +15,25 @@ class Blog(models.Model):
 
     class Meta:
         db_table = "blog"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "tag"
+
+
+class Blog_Tag(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.blog)
+
+    class Meta:
+        db_table = "blog_tag"
+        unique_together = ("blog", "tag")
