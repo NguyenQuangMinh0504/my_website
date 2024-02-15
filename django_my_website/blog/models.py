91bde9377e5cabe05818cfaf5bae805bc788cd6d
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-15 12:55:07 +0700

    .

---
+++ django_my_website/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    snippet = models.CharField(max_length=255)
+    content = models.TextField()
+    total_view = models.SmallIntegerField(default=0)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "blog"
