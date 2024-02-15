33173c1254a9211a18de4c0d0f970c80c98c23af
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-02-15 21:00:49 +0700

    .

--- django_my_website/django_my_website/settings.py
+++ django_my_website/django_my_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from config import DB_HOST
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'PASSWORD': "qmqmqm8c3",
         "NAME": "blog",
+        "HOST": DB_HOST,
     }
 }
 
