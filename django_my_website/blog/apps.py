e36c6ea8c9acdc34cd833615e1c457570fc607b1
NguyenQuangMinh0504 <ngquangminh05042001@gmail.com>
2024-03-15 14:50:27 +0700

    update stuff

--- django_my_website/blog/apps.py
+++ django_my_website/blog/apps.py
@@ -1,6 +1,8 @@
+"""BLog App Config"""
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """Blog App Config"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
